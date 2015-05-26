5107ac012a9e94694390c14ae351b463cc8d3d78
Vladimir Migashko <migashko@gmail.com>
2015-05-27 02:41:48 +0300

    basic_io

--- server/domain/server_tcp.cpp
+++ server/domain/server_tcp.cpp
@@ -1,6 +1,16 @@
 #include "server_tcp.hpp"
-
+#include <iow/ip/tcp/server/server.hpp>
 namespace wfc{
+
+class server_tcp_impl
+  : public ::iow::ip::tcp::server::server<>
+{
+public:
+  typedef server::io_service_type io_service_type;
+  server_tcp_impl(io_service_type& io)
+    : server(io)
+  {}
+};
 
 server_tcp::~server_tcp()
 {
@@ -8,6 +18,22 @@ server_tcp::~server_tcp()
 
 server_tcp::server_tcp()
 {
+}
+
+void server_tcp::start(const std::string& arg)
+{
+  if ( auto g = this->global() )
+  {
+    _impl = std::make_shared<server_tcp_impl>( g->io_service );
+    auto opt = this->options();
+    opt.host = "0.0.0.0";
+    opt.port = "12345";
+    _impl->start( std::move(opt) );
+  }
+  else
+  {
+    domain_object::start(arg);
+  }
 }
 
 }
--- server/domain/server_tcp.hpp
+++ server/domain/server_tcp.hpp
@@ -1,9 +1,14 @@
 #pragma once
+
+#include "server_tcp_config.hpp"
 #include <wfc/domain_object.hpp>
 #include <wfc/iinterface.hpp>
-#include "server_tcp_config.hpp"
+#include <string>
+#include <memory>
 
 namespace wfc{
+
+class server_tcp_impl;
 
 class server_tcp
   : public ::wfc::domain_object<iinterface, server_tcp_config>
@@ -11,6 +16,9 @@ class server_tcp
 public:
   virtual ~server_tcp();
   server_tcp();
+  virtual void start(const std::string&);
+private:
+  std::shared_ptr<server_tcp_impl> _impl;
 };
 
 }
