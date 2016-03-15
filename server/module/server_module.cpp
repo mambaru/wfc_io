eb4422755667c7061bbb53483d8ece12e2ebaf14
Vladimir Migashko <migashko@gmail.com>
2016-03-16 01:14:26 +0300

    upd

--- server/module/server_module.cpp
+++ server/module/server_module.cpp
@@ -2,21 +2,23 @@
 #include "server_module.hpp"
 #include "server_multiton.hpp"
 #include <wfc/module/component_list.hpp>
-#include <wfc/json.hpp>
+#include <wfc/name.hpp>
 
 namespace wfc{
-  
-JSON_NAME2(server_module_name, "server")
 
-class server_module_impl: public ::wfc::component_list<
-  server_module_name,
-  server_multiton
->
+namespace
 {
-};
+  WFC_NAME2(server_module_name, "server")
+  class impl: public ::wfc::component_list<
+    server_module_name,
+    server_multiton
+  >
+  {
+  };
+}
 
 server_module::server_module()
-  : module( std::make_shared<server_module_impl>() )
+  : module( std::make_shared<impl>() )
 {
 }
 
