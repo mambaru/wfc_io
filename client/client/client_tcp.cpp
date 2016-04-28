98324b1d6deae57647ed7d831e4df29ea59f888f
Vladimir Migashko <migashko@gmail.com>
2016-04-28 20:28:04 +0300

    client upd

--- client/client/client_tcp.cpp
+++ client/client/client_tcp.cpp
@@ -28,6 +28,7 @@ public:
     : _id ( ::iow::io::create_id<io_id_t>() )
     , _src(src)
   {
+    if (src.lock()==nullptr) abort();
     _client = std::make_shared<client_type>(io);
   }
   
@@ -42,11 +43,27 @@ public:
    
     if ( _src.lock() != nullptr )
     {
+      if ( opt.connection.incoming_handler == nullptr )
+      {
+        opt.connection.incoming_handler = [pthis](data_ptr d, io_id_t, outgoing_handler_t handler)
+        {
+          if ( auto src = pthis->_src.lock() )
+          {
+            src->perform_io(std::move(d), pthis->_id, std::move(handler) );
+          }
+        };
+      }
+      else
+      {
+      }
+
       auto connect_handler = opt.connect_handler;
       opt.connect_handler = [pthis, connect_handler]()
       {
         if ( auto src = pthis->_src.lock() )
+        {
           src->reg_io( pthis->_id, pthis );
+        }
         if ( connect_handler ) connect_handler();
       };
       
@@ -57,6 +74,10 @@ public:
           src->unreg_io( pthis->_id);
         if ( error_handler ) error_handler(ec);
       };
+    }
+    else
+    {
+      IOW_LOG_WARNING("client_tcp::client::start src not set")
     }
     
     _client->start(opt);
@@ -76,9 +97,9 @@ public:
 
   virtual void perform_io( iinterface::data_ptr d, io_id_t /*io_id*/, outgoing_handler_t handler) override
   {
-    DEBUG_LOG_MESSAGE("client_tcp:client " << d)
     if ( auto rd = _client->send( std::move(d) ) )
     {
+      DEBUG_LOG_ERROR("tcp_client send FAIL: " << d)
       if (handler!=nullptr)
         handler( nullptr );
     }
@@ -87,6 +108,12 @@ public:
   iinterface_ptr get_src() const
   {
     return _src;
+  }
+  
+  void set_src(iinterface_ptr src)
+  {
+    if (src.lock()==nullptr) abort();
+    _src = src;
   }
 
 private:
@@ -97,16 +124,41 @@ private:
 
 }
 
-class client_tcp::impl
+
+/*class client_handler: public iinterface
 {
 public:
+  outgoing_handler_t handler)
+  
+};
+*/
+
+class client_tcp::impl
+{
+  
+public:
+  
+  class client_handler: public iinterface
+  {
+  public:
+    client_handler(outgoing_handler_t handler): _handler(handler) {}
+    virtual void perform_io( iinterface::data_ptr d, io_id_t /*id*/, outgoing_handler_t /*handler*/) override
+    {
+      _handler( std::move(d) );
+    }
+  private:
+    outgoing_handler_t _handler;
+  };
+  
   typedef std::mutex mutex_type;
   typedef client::io_id_t io_id_t;
   typedef std::shared_ptr<client> client_ptr;
   typedef client::io_service_type io_service_type;
-  typedef std::map< io_id_t, client_ptr> client_map_t;
-  typedef ::iow::ip::tcp::client::options options_type;
   typedef std::weak_ptr< ::wfc::iinterface > iinterface_ptr;
+  typedef std::pair<client_ptr, std::shared_ptr<::wfc::iinterface> > client_pair;
+  typedef std::map< io_id_t, client_pair> client_map_t;
+  typedef ::iow::ip::tcp::client::options options_type;
+  
   
   impl( io_service_type& io)
     : _io(io)
@@ -117,12 +169,13 @@ public:
   {
     std::lock_guard<mutex_type> lk(_mutex);
     _opt = opt;
-    for ( auto& cli : _clients )
+    for ( auto& item : _clients )
     {
-      auto src = cli.second->get_src();
-      cli.second->stop();
-      cli.second = std::make_shared<client>(_io, src);
-      cli.second->start(opt);
+      auto cli = item.second.first;
+      auto src = cli->get_src();
+      cli->stop();
+      item.second.first = std::make_shared<client>(_io, src);
+      cli->start(opt);
     }
   }
   
@@ -130,15 +183,15 @@ public:
   {
     for ( auto& cli : _clients )
     {
-      cli.second->stop();
-      cli.second = nullptr;
+      cli.second.first->stop();
+      cli.second.first = nullptr;
     }
     _clients.clear();
   }
   
   void perform_io( iinterface::data_ptr d, io_id_t id, outgoing_handler_t handler)
   {
-    if ( auto cli = this->queryset(id, iinterface_ptr() ) )
+    if ( auto cli = this->queryset(id, handler ) )
     {
       cli->perform_io( std::move(d), id, std::move(handler) );
     }
@@ -153,15 +206,34 @@ public:
     std::lock_guard<mutex_type> lk(_mutex);
     return this->find_(id);
   }
-  
-  client_ptr queryset( io_id_t id, iinterface_ptr src)
+
+  client_ptr queryset( io_id_t id, outgoing_handler_t handler)
+  {
+    std::lock_guard<mutex_type> lk(_mutex);
+    client_ptr cli = this->find_(id);
+    if ( cli == nullptr )
+    {
+      auto hdr = std::make_shared<client_handler>(handler);
+      cli = std::make_shared<client>(_io, hdr);
+      _clients.insert( std::make_pair(id, std::make_pair(cli, hdr) ) );
+      cli->start(_opt);
+    }
+    return cli;
+  }
+
+  client_ptr upsert( io_id_t id, iinterface_ptr src)
   {
     std::lock_guard<mutex_type> lk(_mutex);
     client_ptr cli = this->find_(id);
     if ( cli == nullptr )
     {
       cli = std::make_shared<client>(_io, src);
-      _clients.insert( std::make_pair(id, cli) );
+      _clients.insert( std::make_pair(id, std::make_pair(cli, nullptr)) );
+      cli->start(_opt);
+    }
+    else
+    {
+      cli->set_src(src);
     }
     return cli;
   }
@@ -172,7 +244,7 @@ public:
     auto itr = _clients.find(id);
     if ( itr == _clients.end() )
       return nullptr;
-    auto cli = itr->second;
+    auto cli = itr->second.first;
     _clients.erase(itr);
     return cli;
   }
@@ -183,7 +255,7 @@ private:
   {
     auto itr = _clients.find(id);
     if ( itr!=_clients.end() )
-      return itr->second;
+      return itr->second.first;
     return nullptr;
   }
   
@@ -247,18 +319,19 @@ void client_tcp::stop(const std::string&)
 
 void client_tcp::reg_io(io_id_t io_id, std::weak_ptr<iinterface> itf)
 {
-  this->_impl->queryset( io_id, itf);
+  DEBUG_LOG_TRACE("client_tcp::reg_io id=" << io_id)
+  this->_impl->upsert( io_id, itf);
 }
 
 void client_tcp::unreg_io(io_id_t io_id)
 {
   // Обязательно удаляем обработчик при закрыии источника
+  DEBUG_LOG_TRACE("client_tcp::unreg_io id=" << io_id)
   _impl->erase(io_id);
 }
 
 void client_tcp::perform_io(data_ptr d, io_id_t io_id, outgoing_handler_t handler) 
 {
-  DEBUG_LOG_MESSAGE("client_tcp: " << d)
   _impl->perform_io( std::move(d), io_id, std::move(handler) );
 }
 
