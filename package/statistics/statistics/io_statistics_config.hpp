fdaf979da3341af4c8999ffc30c73e0effcc860d
Vladimir Migashko <migashko@gmail.com>
2016-12-05 20:04:28 +0300

    stat conf fix

--- package/statistics/statistics/io_statistics_config.hpp
+++ package/statistics/statistics/io_statistics_config.hpp
@@ -5,8 +5,8 @@ namespace wfc{
 struct io_statistics_config
 {
   std::string target;
-  std::string time_name;
-  std::string size_name;
+  std::string time_name = "time";
+  std::string size_name = "size";
 };
 
 }
