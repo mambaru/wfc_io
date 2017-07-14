1f51cb6bd0ea03f3a8f0cb8de1ee440d7c98d02d
Vladimir Migashko <migashko@gmail.com>
2017-07-14 18:49:00 +0300

    *wfc_demo

--- package/server/server_tcp/server_tcp_config.hpp
+++ package/server/server_tcp/server_tcp_config.hpp
@@ -1,24 +1,14 @@
 #pragma once
 
-#include <iow/io/aux/data_pool_options.hpp>
 #include "tcp_options.hpp"
 #include <set>
 
 namespace wfc{ namespace io{
-  
-  /*
-struct data_pool_options: 
-  ::iow::io::data_pool_options 
-{
-  bool disabled = false;
-};*/
 
 struct server_tcp_config: tcp_options
 {
   std::string target;
   bool keep_alive = true;
-  //data_pool_options data_pool;
-  /*std::set<int> cpu;*/
 };
 
 }}
--- package/server/server_tcp/server_tcp_config_json.hpp
+++ package/server/server_tcp/server_tcp_config_json.hpp
@@ -2,7 +2,6 @@
 
 #include "server_tcp_config.hpp"
 #include <iow/ip/tcp/server/options_json.hpp>
-#include <iow/io/aux/data_pool_options_json.hpp>
 #include <wfc/json.hpp>
 
 namespace wfc{ namespace io{
@@ -22,24 +21,6 @@ struct connection_options_json
   typedef type::member_list member_list;
 };
 
-/*
-struct data_pool_options_json
-{
-  JSON_NAME(disabled)
-  typedef json::object<
-    data_pool_options,
-    json::member_list<
-      json::member<n_disabled, data_pool_options, bool, &data_pool_options::disabled>,
-      json::base< ::iow::io::data_pool_options_json >
-    >,
-    json::strict_mode
-  > type;
-
-  typedef type::target target;
-  typedef type::serializer serializer;
-  typedef type::member_list member_list;
-};
-*/
 
 struct server_tcp_config_json
 {
@@ -50,7 +31,6 @@ struct server_tcp_config_json
   
   JSON_NAME(target)
   JSON_NAME(cpu)
-  JSON_NAME(data_pool)
   JSON_NAME2(n_keep_alive, "keep-alive")
 
   typedef json::object<
@@ -58,9 +38,7 @@ struct server_tcp_config_json
     json::member_list<
         json::member<n_target, server_tcp_config, std::string, &server_tcp_config::target>,
         json::member<n_keep_alive, server_tcp_config, bool, &server_tcp_config::keep_alive>,
-        /*json::member<n_cpu, server_tcp_config, std::set<int>, &server_tcp_config::cpu, json::array< std::set< json::value<int> >  > >,*/
-        json::base< server_json >/*,
-        json::member<n_data_pool, server_tcp_config, data_pool_options, &server_tcp_config::data_pool, data_pool_options_json>*/
+        json::base< server_json >
     >,
     json::strict_mode
   > type;
