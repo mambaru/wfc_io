74fd1111daaa7d39ccf93d10374a08dc71784d76
Vladimir Migashko <migashko@gmail.com>
2017-08-09 00:57:57 +0300

    -data_pool

--- package/server/server_tcp/server_tcp.hpp
+++ package/server/server_tcp/server_tcp.hpp
@@ -3,7 +3,6 @@
 #include "server_tcp_config.hpp"
 #include <wfc/domain_object.hpp>
 #include <wfc/iinterface.hpp>
-//#include <iow/io/aux/data_pool.hpp>
 #include <string>
 #include <memory>
 
@@ -14,7 +13,6 @@ class server_tcp
   , public std::enable_shared_from_this<server_tcp>
 {
   class impl;
-  typedef ::iow::io::data_pool<data_type> data_pool;
 public:
   virtual ~server_tcp();
   server_tcp();
@@ -24,7 +22,6 @@ public:
 private:
   std::shared_ptr<impl> _impl;
   std::weak_ptr<iinterface> _target;
-//  std::shared_ptr<data_pool> _data_pool;
 };
 
 }}
