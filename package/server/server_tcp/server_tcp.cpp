c8ce5e4573b6ab6f60bb33587d14534086b7964e
Vladimir Migashko <migashko@gmail.com>
2018-03-20 18:56:35 +0300

    tcp bind exception fix

--- package/server/server_tcp/server_tcp.cpp
+++ package/server/server_tcp/server_tcp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 #include "server_tcp.hpp"
 #include "tcp_acceptor.hpp"
 #include <wfc/logger.hpp>
@@ -142,7 +143,14 @@ void server_tcp::start()
       };
     }
 
-    _impl->start( opt );
+    try
+    {
+      _impl->start( opt );
+    }
+    catch(const std::exception& e)
+    {
+      DOMAIN_LOG_FATAL( "server_tcp port: " << this->options().port << " error: " << e.what() )
+    }
   }
 }
 
